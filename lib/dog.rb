a1c9fbbe5e2069eb2e431e9963972e9ce73c89ca
dickm19 <maryclairedick@gmail.com>
2020-09-23 20:48:23 -0400

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,23 @@
-class Dog
+# class Dog
+
+# end
+
+
+# sql = <<-SQL
+# CREATE TABLE IF NOT EXISTS dogs (
+#     id INTEGER PRIMARY KEY,
+#     name TEXT
+#     breed TEXT
+#     )
+# SQL
+
+            
+
+# ActiveRecord::Base.connection.execute(sql)
+#             ActiveRecord::Base.establish_connection(
+#                 :adapter => "sqlite3",
+#                 :database => "db/dogs.sqlite"
+#                 )
+
+class Dog < ActiveRecord::Base
 end
